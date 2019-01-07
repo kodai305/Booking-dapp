6eb23dcfbc4e366739037a303139720e4536437d
Kodai Takagi <mj5824@icloud.com>
2019-01-07 09:41:23 +0900

    change utils

--- frontend/src/utils/myweb3.js
+++ frontend/src/utils/myweb3.js
@@ -1,7 +1,7 @@
 import Web3 from 'web3';
 
 var web3 = null;
-if (typeof(window.web3) !== 'undefined' || window.web3 != null) {
+if (typeof(window) !== 'undefined' || window != null) {
     web3 = new Web3(window.web3.currentProvider);
 }
 export default web3;
